ec40a2098c7ef14601a3726fe0c5c4430f184e15
kemmo47 <longkem123456@gmail.com>
2022-08-24 23:21:13 +0700

    Post api create todo

--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 type Todo struct {
@@ -18,6 +19,11 @@ func main() {
 	fmt.Print("Welcome to the OpenStack Project Project")
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	todos := []Todo{}
 
